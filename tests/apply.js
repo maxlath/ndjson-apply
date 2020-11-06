00f833972f3c11aff8d633a11eb6d6d6da7bbd81
maxlath <k@maxlath.eu>
2020-11-06 17:24:41 +0100

    tests: lint

--- tests/apply.js
+++ tests/apply.js
@@ -6,7 +6,7 @@ const shouldntGetHere = () => { throw new Error("shouldn't get here") }
 describe('apply', () => {
   it('should reject an invalid function module', async () => {
     try {
-      const { stdout, stderr } = await exec('./bin/ndjson-apply')
+      await exec('./bin/ndjson-apply')
       shouldntGetHere()
     } catch (err) {
       err.message.should.startWith('Command failed')
@@ -14,7 +14,7 @@ describe('apply', () => {
   })
 
   it('should accept a sync function', async () => {
-    const { stdout, stderr } = await exec('./bin/ndjson-apply ./tests/assets/sync_transformer.js < ./tests/assets/sample.ndjson')
+    const { stdout } = await exec('./bin/ndjson-apply ./tests/assets/sync_transformer.js < ./tests/assets/sample.ndjson')
     const data = stdout.trim().split('\n').map(line => JSON.parse(line))
     data.should.deepEqual([
       { a: 123, b: 456, total: 579 },
@@ -23,7 +23,7 @@ describe('apply', () => {
   })
 
   it('should accept an async function', async () => {
-    const { stdout, stderr } = await exec('./bin/ndjson-apply ./tests/assets/async_transformer.js < ./tests/assets/sample.ndjson')
+    const { stdout } = await exec('./bin/ndjson-apply ./tests/assets/async_transformer.js < ./tests/assets/sample.ndjson')
     const data = stdout.trim().split('\n').map(line => JSON.parse(line))
     data.should.deepEqual([
       { a: 123, b: 456, total: 679 },
@@ -32,7 +32,7 @@ describe('apply', () => {
   })
 
   it('should filter-out lines returning empty', async () => {
-    const { stdout, stderr } = await exec('./bin/ndjson-apply ./tests/assets/even_only_transformer.js < ./tests/assets/sample.ndjson')
+    const { stdout } = await exec('./bin/ndjson-apply ./tests/assets/even_only_transformer.js < ./tests/assets/sample.ndjson')
     const data = stdout.trim().split('\n').map(line => JSON.parse(line))
     data.should.deepEqual([
       { a: 789, b: 123, total: 912 }
--- tests/diff.js
+++ tests/diff.js
@@ -1,19 +1,18 @@
 require('should')
 const { promisify } = require('util')
 const exec = promisify(require('child_process').exec)
-const shouldntGetHere = () => { throw new Error("shouldn't get here") }
 const { readFileSync } = require('fs')
 const sampleSyncTransformerDiff = readFileSync('./tests/assets/sample.sync_transformer.diff').toString()
 const sampleAsyncTransformerDiff = readFileSync('./tests/assets/sample.async_transformer.diff').toString()
 
 describe('diff', () => {
   it('should output a diff for a sync function', async () => {
-    const { stdout, stderr } = await exec('./bin/ndjson-apply --diff ./tests/assets/sync_transformer.js < ./tests/assets/sample.ndjson')
+    const { stdout } = await exec('./bin/ndjson-apply --diff ./tests/assets/sync_transformer.js < ./tests/assets/sample.ndjson')
     stdout.should.equal(sampleSyncTransformerDiff)
   })
 
   it('should output a diff for an async function', async () => {
-    const { stdout, stderr } = await exec('./bin/ndjson-apply --diff ./tests/assets/async_transformer.js < ./tests/assets/sample.ndjson')
+    const { stdout } = await exec('./bin/ndjson-apply --diff ./tests/assets/async_transformer.js < ./tests/assets/sample.ndjson')
     stdout.should.equal(sampleAsyncTransformerDiff)
   })
 })
